f56cd73b1a14d8c010520469e158a84081bd89ff
isma <xwarzmiixz@gmail.com>
2024-05-17 12:31:41 +0200

    Componente Head e inicio del ejercicio

--- src/services/ejercicios.ts
+++ src/services/ejercicios.ts
@@ -3,3 +3,9 @@ export const getEjercicios = async () => {
 
   return result.json();
 };
+
+export const getEjercicio = async (id: Number | undefined) => {
+  const result = await fetch(`http://localhost:3000/ejercicios/${id}`);
+
+  return result.json();
+};
