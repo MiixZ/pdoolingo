80495b429868059aba45e61a63c0917fbc2cbf36
isma <xwarzmiixz@gmail.com>
2024-05-20 19:08:57 +0200

    Modularización servicio de usuario

--- src/interfaces/Usuario.ts
+++ src/interfaces/Usuario.ts
@@ -5,4 +5,6 @@ export interface Usuario {
   DNI: string;
   vidas: number;
   tipo: string;
+  image: string | null | undefined;
+  email: string | null | undefined;
 }
--- src/services/login.ts
+++ src/services/login.ts
@@ -1,3 +1,32 @@
+import { getSession } from "auth-astro/server";
+import { getUsuario } from "./usuario";
+import type { Usuario } from "@interfaces/Usuario";
+
+export const getSesion = async (
+  req: Request
+): Promise<Usuario | null | false> => {
+  const session = await getSession(req);
+  if (!session) {
+    return false;
+  }
+
+  const sessionUser = session ? session.user : null;
+  const { email, name, image } = sessionUser || {};
+
+  const nombre = name?.split(" ")[0];
+  const apellidos = name?.split(" ").slice(1).join(" ");
+  const usuarios = await getUsuario(nombre, apellidos);
+
+  if (usuarios == null) {
+    return null;
+  }
+
+  const usuario = usuarios[0];
+  usuario.email = email;
+  usuario.image = image;
+
+  return usuario;
+};
 export interface Login {
   email: string;
   password: string;
