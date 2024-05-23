d45ef9c2fc0ad3373f4109431ba0eeb804f474bf
isma <xwarzmiixz@gmail.com>
2024-05-23 19:57:12 +0200

    Lógica de ejercicios terminada.

--- src/interfaces/Respuesta.ts
+++ src/interfaces/Respuesta.ts
@@ -1,4 +1,5 @@
 export interface Respuesta {
   id?: number;
   texto: string;
+  correcta?: boolean;
 }
--- src/services/ejercicios.ts
+++ src/services/ejercicios.ts
@@ -1,5 +1,7 @@
 import type { Ejercicio } from "@interfaces/Ejercicio";
 import { url } from "./comun";
+import type { Respuesta } from "@interfaces/Respuesta";
+import type { Data } from "@interfaces/Data";
 
 export const getEjercicios = async () => {
   const result = await fetch(url + "ejercicios");
@@ -17,15 +19,58 @@ export const getEjercicio = async (
 
 export const insertEjercicio = async (
   ejercicio: Ejercicio,
-  respuesta: {}
-) => {
-  const result = await fetch(url + "ejercicios", {
+  respuestas: Respuesta[]
+): Promise<void> => {
+  console.log("Ejercicio a crear: ", ejercicio);
+  console.log("Respuestas a crear: ", respuestas);
+  const resultEjercicio = await fetch(url + "ejercicios", {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
     },
-    body: JSON.stringify({ ...ejercicio, respuesta }),
+    body: JSON.stringify({ ...ejercicio }),
   });
 
-  return result.json();
+  const responseEjercicio = (await resultEjercicio.json()) as Data;
+  const ejercicioCreado = responseEjercicio.data as Ejercicio;
+
+  console.log("Ejercicio creado: ", ejercicioCreado);
+
+  let respuestasCreadas: Respuesta[] = [];
+
+  respuestas.map(async (respuesta) => {
+    const resultRespuesta = await fetch(url + "respuestas", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ texto: respuesta.texto }),
+    });
+
+    const response = (await resultRespuesta.json()) as Data;
+    const respuestaCreada = response.data as Respuesta;
+
+    console.log("Respuesta creada: ", respuestaCreada);
+
+    if (response.success) {
+      respuestasCreadas.push(respuestaCreada);
+
+      const resultAsignadas = await fetch(url + "ejercicios-respuestas", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          id_ejercicio: ejercicioCreado.id,
+          id_respuesta: respuestaCreada.id,
+          es_correcta: respuesta.correcta,
+        }),
+      });
+
+      const responseAsignadas = (await resultAsignadas.json()) as Data;
+      const asignadaCreada = responseAsignadas.data;
+
+      console.log("Asignada creada: ", asignadaCreada);
+    }
+  });
 };
