3d6824f6e000ee34be26cfc7d5a6341a5dc81205
isma <xwarzmiixz@gmail.com>
2024-05-28 17:56:42 +0200

    Contador pasado a react

---
+++ src/components/ejercicios/in/contador.jsx
@@ -0,0 +1,23 @@
+import React, { useState, useEffect } from "react";
+
+const Contador = () => {
+  const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCount((prevCount) => prevCount + 1);
+    }, 1000);
+
+    return () => {
+      clearInterval(interval);
+    };
+  }, []);
+
+  return (
+    <div class="flex gap-4 mt-4 text-2xl text-white sm:w-full lg:w-1/2 xl:w-1/3 w-1/4">
+      <h1>Tiempo transcurrido: {count}</h1>
+    </div>
+  );
+};
+
+export default Contador;
